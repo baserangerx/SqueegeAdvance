e515381755b06ea669426ffff09046695cc3211e
baserangerx <evan.k.landry@gmail.com>
2025-01-11 23:37:13 -0800

    added sewer image

---
+++ include/sewer.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(sewer)
+
+//======================================================================
+//
+//	sewer, 240x160@4, 
+//	+ palette 16 entries, not compressed
+//	+ 600 tiles Metatiled by 30x20 not compressed
+//	Total size: 32 + 19200 = 19232
+//
+//	Time-stamp: 2025-01-11, 23:34:15
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SEWER_H
+#define GRIT_SEWER_H
+
+#define sewerTilesLen 19200
+extern const unsigned int sewerTiles[4800];
+
+#define sewerPalLen 32
+extern const unsigned short sewerPal[16];
+
+#endif // GRIT_SEWER_H
+
+//}}BLOCK(sewer)
--- source/images/squeege.c
+++ source/images/squeege.c
@@ -8,7 +8,7 @@
 //	+ 96 tiles Metatiled by 4x4 not compressed
 //	Total size: 32 + 3072 = 3104
 //
-//	Time-stamp: 2025-01-08, 23:59:02
+//	Time-stamp: 2025-01-11, 23:33:28
 //	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
 //	( http://www.coranac.com/projects/#grit )
 //
